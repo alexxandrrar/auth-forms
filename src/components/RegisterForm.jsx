a9df7225669a91266370b39622f7a90ff1618d6c
alexxandrrar <alexandroovnaa@gmail.com>
2022-08-26 00:00:43 +0300

    feature/ added-validation

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useFormik } from "formik";
 import "../index.css";
+import * as Yup from "yup";
 
 const RegisterForm = () => {
   const formik = useFormik({
@@ -9,6 +10,17 @@ const RegisterForm = () => {
       email: "",
       password: "",
     },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(15, "Must be under 15 symbols")
+        .min(2, "Must be more than 2 symbols")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .required("No password provided.")
+        .min(8, "Password is too short - should be 8 chars minimum.")
+        .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+    }),
     onSubmit: (values) => {
       console.log(JSON.stringify(values, null, 2));
     },
@@ -26,6 +38,9 @@ const RegisterForm = () => {
           onChange={handleChange}
           value={values.name}
         />
+        {formik.touched.name && formik.errors.name ? (
+          <div>{formik.errors.name}</div>
+        ) : null}
       </div>
       <div className="form-item">
         <label htmlFor="email">Email</label>
@@ -36,6 +51,9 @@ const RegisterForm = () => {
           onChange={handleChange}
           value={values.email}
         />
+        {formik.touched.email && formik.errors.email ? (
+          <div>{formik.errors.email}</div>
+        ) : null}
       </div>
       <div className="form-item">
         <label htmlFor="password">Password</label>
@@ -46,6 +64,9 @@ const RegisterForm = () => {
           onChange={handleChange}
           value={values.password}
         />
+        {formik.touched.password && formik.errors.password ? (
+          <div>{formik.errors.password}</div>
+        ) : null}
       </div>
       <button className="sumbit-button" type="submit">
         Submit
